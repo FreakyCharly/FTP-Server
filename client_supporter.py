c864872b3aa7d422fe6add7e16081ee0b8604fb1
FTP-Server <c.anivarrob@gmail.com>
2022-03-09 23:10:27 +0100

    Implementado el algoritmo de recepción de petición del cliente. Implementadas las funciones de atención USER; PASS, NOOP y PORT. Las funciones QUIT y LIST están semiacabadas (QUIT debe finalizar el proceso hilo; LIST debe obtener del SO lo de '1 user group'). Falta generr un control de usuario para que no se puedan ejecutar instrucciones sin haber hecho login

--- client_supporter.py
+++ client_supporter.py
@@ -28,10 +28,26 @@ FTP Model by RFC.                       |/--------\|
  ----------   |\------/|                |\--------/|  ----------
               ----------                -------------
                 Server-FTP                   User-FTP
+References:
+https://www.adamsmith.haus/python/answers/how-to-call-a-function-by-its-name-as-a-string-in-python
+
 """
 
 
-import socket, threading, os
+import threading, os, socket, time
+
+
+cmds_available = {
+    'USER': True,
+    'PASS': True,
+    'CWD': True,
+    'REIN': True,
+    'QUIT': True,
+    'CDUP': True,
+    'ACCT': True,
+    'NOOP': True
+}
+
 
 class ClientSupporter(threading.Thread):
     """
@@ -43,29 +59,61 @@ class ClientSupporter(threading.Thread):
       --> ClientSupporter calls for 'recv()' and waits for client actions, repeatedly.
         --> ClientSupporter will end when QUIT command is received.
     """
-    # CLIENT_MAX_SIZE_MSG = 1024  # Buffer for client messages
-
+    CLIENT_MAX_SIZE_MSG = 256  # Buffer for client messages
+    DEFAULT_DATA_PORT = 20
+    
 
     def __init__(self, client_connection, client_address):
-        self.client_conn = client_connection
-        self.client_addr = client_address  # Both IP and Port number from client
+        self.cli_conn = client_connection
+        self.cli_addr = client_address  # Both IP and Port number from client
+        self.data_addr = client_address[0]
+        self.data_port = self.DEFAULT_DATA_PORT
+
+        self.parse = lambda a: a + '\r\n'
+
+        self.user = None
+        self.root_dir = os.path.abspath('.')
+        self.curr_dir = self.root_dir
 
         threading.Thread.__init__(self)
     
     def run(self):
-        """
-        Idea of algorithm to be implemented:
+        conn = self.cli_conn
+        parse = self.parse
 
-        1. Welcome the client (as especify in the RFC) (use of send())
-        2. Infinite loop:
-            2.1. Get the message from client (use of recv())
-                2.1.1. If there's a message
-                    2.1.1.1. Parse the message to separate command from arguments
-                    2.1.1.2. Call the corresponding callback and send arguments if needed (try-except)
-                        2.1.1.2.1. Send a 5XX error to user if callback makes an exception
-                2.1.2. Otherwise
-                    2.1.2.1. Break loop and exit (stop the thread)   
-        """
+        conn.send(parse('220 Carlos FTP Server (Version 0.5) ready'))
+        while True:
+            req = conn.recv(self.CLIENT_MAX_SIZE_MSG)
+            if req:
+                try:
+                    # Get method callback
+                    f = ''.join(req[:4])
+                    
+                    # If not recognised, respond and exit
+                    if f not in cmds_available:
+                        conn.send(parse('502 Method not implemented'))
+                        break
+                    
+                    # Get callback as function
+                    func = getattr(self, f.upper())
+
+                    # Get params
+                    txt = ''
+                    if len(req) > 4:
+                        txt = req[5:]
+
+                    # Call function with its params
+                    if len(txt) > 0:
+                        func(txt)
+                    else:
+                        func()
+                
+                except:
+                    # Function is recognised but couldn't be called properly
+                    conn.send(parse('501 Parameter syntax error'))
+            
+            else:
+                break
     
     """
     To be implemented (callbacks):
@@ -81,3 +129,77 @@ class ClientSupporter(threading.Thread):
       --> Transfer callbacks
             - 
     """
+    def USER(self, msg):
+        """
+        User log-in.
+        """
+        self.user = msg
+        self.cli_conn.send(self.parse('331 Password needed.'))
+    
+    def PASS(self, msg):
+        """
+        User password.
+        Needed after USER.
+        """
+        self.cli_conn.send(self.parse(f'230 {self.user} connected.'))
+    
+    def QUIT(self):
+        """
+        Quit user, hence thread dies.
+        """
+        self.cli_conn.send(self.parse(f'221 Bye, {self.user}.'))
+    
+    def NOOP(self):
+        """
+        No operation.
+        """
+        self.cli_conn.send(self.parse('220 OK.'))
+    
+    def PORT(self, msg):
+        """
+        Set a PORT and a IP to create future Data channels.
+        """
+        data = msg.split(',')
+        try:
+            self.data_addr = '.'.join(data[:4])                 # Get IP
+            self.data_port = (int(data[4]) << 8) + int(data[5]) # Get port
+        except:
+            self.cli_conn.send(self.parse('501 Parameter syntax error'))
+        
+        self.cli_conn.send(self.parse('200 PORT succesful.'))
+
+    def LIST(self):
+        """
+        Returns same format as 'ls -l' from UNIX systems.
+        """
+        # Open data connection
+        self.cli_conn.send(self.parse(f'150 Opening ASCII mode data connection for {self.curr_dir}.'))
+        self.data_conn=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        try:
+            self.data_conn.connect((self.data_addr,self.data_port))
+        except:
+            self.cli_conn.send(self.parse('425 Data conection cannot be opened.'))
+        
+        # Make up a 'ls -l' format type
+        os.chdir(self.curr_dir)
+        listed = os.listdir('.')
+        item_parsed = ''
+        base_mode = 'rwx'*3
+        for item in listed:
+            s = os.stat(item)
+            final_mode = ''
+
+            # Guess the 'r', 'w', 'x', '-' mode
+            for i in len(base_mode):
+                final_mode += ((s.st_mode >> (8-i)) & 1) and base_mode[i] or '-'
+            
+            # Type of file ('d' -> Directory, '-' -> Any other)
+            dir = 'd' if os.path.isdir(item) else '-'
+            
+            # Date
+            t = time.strftime(' %b %d %H:%M ', time.gmtime(s.st_mtime))
+            
+            # Parse full item
+            item_parsed = dir + final_mode + ' 1 user group ' + str(s.st_size) + t + os.path.basename(item)
+            # TODO '1 user group' must be obtained from os
+            self.data_conn.send(self.parse(item_parsed))
--- ftp_server.py
+++ ftp_server.py
@@ -3,12 +3,12 @@
 import socket, threading
 
 class FTPServer(threading.Thread):
-    CONTROL_PORT = 21
+    DEFAULT_CONTROL_PORT = 21
     LISTEN_QUEUE = 5
 
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind(('', FTPServer.CONTROL_PORT))
+        self.socket.bind(('', FTPServer.DEFAULT_CONTROL_PORT))
         threading.Thread.__init__(self)
     
     def run(self):
